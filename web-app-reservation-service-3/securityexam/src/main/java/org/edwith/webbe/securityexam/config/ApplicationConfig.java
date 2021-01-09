622517b3b47abe6d9fbf2de57379c99e76c09f77
JongJin Bae <qowhdwlsdk@gmail.com>
2021-01-09 22:44:03 +0900

    Add Security Example Project Used in Spring Security Lecture

---
+++ web-app-reservation-service-3/securityexam/src/main/java/org/edwith/webbe/securityexam/config/ApplicationConfig.java
@@ -0,0 +1,55 @@
+package org.edwith.webbe.securityexam.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+// 레이어드 아키텍처에서 Controller가 사용하는 Bea들에 대해 설정을 한다.
+// dao, service를 컴포넌트 스캔하여 찾도록 한다.
+// 어노테이션으로 트랜잭션을 관리하기 위해 @EnableTransactionManagement를 설정하였다.
+@Configuration
+@ComponentScan(basePackages = {"org.edwith.webbe.securityexam.dao", "org.edwith.webbe.securityexam.service"})
+@EnableTransactionManagement
+public class ApplicationConfig implements TransactionManagementConfigurer {
+	
+	// mysql Driver 클래스를 메모리에 로드 시키기 위한 변수 지정
+	private String driverClassName = "com.mysql.cj.jdbc.Driver";
+
+	// DB와 커넥션 맺기 위한 정보
+    private String url = "jdbc:mysql://localhost:3306/connectdb?serverTimezone=UTC";
+    private String username = "connectuser";
+    private String password = "connect123!@#";
+    
+    
+    // 커넥션 풀을 잘 조정할 DataSource Bean 등록
+    @Bean
+    public DataSource dataSource() {
+    	BasicDataSource dataSource = new BasicDataSource();
+    	dataSource.setDriverClassName(driverClassName);
+    	dataSource.setUrl(url);
+    	dataSource.setUsername(username);
+    	dataSource.setPassword(password);
+    	
+    	return dataSource;
+    }
+    
+    // 애노테이션 기반의 트랜잭션 관리자를 설정
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+    	return new DataSourceTransactionManager(dataSource());
+    }
+    
+	@Override
+	public TransactionManager annotationDrivenTransactionManager() {
+		return transactionManager();
+	}
+
+}
